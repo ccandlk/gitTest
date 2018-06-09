96a1002333757166553d0a47d02fb4e44d62bd2d
chaihongxia <chaihongxia@meituan.com>
2018-06-09 18:11:19 +0800

    时间控件修改

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">this is my test</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          my name is <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
