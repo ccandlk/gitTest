df0877197e612c8217fb3e658146fb4d2014420c
chaihongxia <chaihongxia@meituan.com>
2018-06-11 17:07:09 +0800

    add read.txt

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">this is my test</h1>
         </header>
         <p className="App-intro">
-          my name is cc<code>src/App.js</code> and save to reload.
+          my name is chx<code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
